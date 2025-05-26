d630eb00cae830c6a7681ff78967f87e894883dd
Gangezwer <gangezwer@gmail.com>
2025-05-26 12:47:05 +0530

    new fix in the model

--- models/country.js
+++ models/country.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const countrySchema = mongoose.Schema(
   {
-    question: {
+    name: {
       en: [
             {
                 name: { type: String, required: true },
