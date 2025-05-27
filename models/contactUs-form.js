72058ae6c2b379b822d213906bb1f19719fdf9be
Gangezwer <gangezwer@gmail.com>
2025-05-27 22:05:04 +0530

    unique issue resolved

--- models/contactUs-form.js
+++ models/contactUs-form.js
@@ -5,12 +5,10 @@ const contactUsFormSchema = new mongoose.Schema(
     firstName: {
       type: String,
       required: true,
-      unique: true,
     },
     lastName: {
       type: String,
       required: true,
-      unique: true,
     },
     email: {
       type: String,
