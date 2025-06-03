5e70e103b0ff09cf76a463ababbc65bb59990c58
Gangezwer <gangezwer@gmail.com>
2025-06-03 18:37:41 +0530

    bg color name added.

--- models/obituraryPostBgColor.js
+++ models/obituraryPostBgColor.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const obituraryPostBgColorSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     colorCode: {
         type: String,
         required: true,
